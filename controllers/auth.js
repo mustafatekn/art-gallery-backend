d067e20f68bd795e092a61a7343a5e06064b3528
Mustafa Osman Tekin <mustafa.tekin@demarkelabs.com>
2022-04-10 16:44:44 +0300

    add sign in function

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,25 +1,29 @@
 const bcrypt = require("bcrypt");
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
-const {isEmpty, isMatched, isEmail} = require('../util/validate');
+const { isEmpty, isMatched, isEmail } = require("../util/validate");
 
 module.exports = {
   signUp: async (req, res) => {
     const { username, email, password, confirmPassword } = req.body;
-    const emptyErrors = isEmpty({username, email, password});
-    const matchedErrors = isMatched({password, confirmPassword});
+
+    const emptyErrors = isEmpty({ username, email, password });
+    const matchedErrors = isMatched({ password, confirmPassword });
     const emailErrors = isEmail(email);
 
-    if(Object.keys(emptyErrors).length > 0) return res.status(400).json(emptyErrors);
-    if(Object.keys(emailErrors).length > 0) return res.status(400).json(emailErrors);
-    if(Object.keys(matchedErrors).length > 0) return res.status(400).json(matchedErrors);
-    
+    if (Object.keys(emptyErrors).length > 0)
+      return res.status(400).json(emptyErrors);
+    if (Object.keys(emailErrors).length > 0)
+      return res.status(400).json(emailErrors);
+    if (Object.keys(matchedErrors).length > 0)
+      return res.status(400).json(matchedErrors);
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = new User({
       username,
       email,
-      password:hashedPassword,
+      password: hashedPassword,
       role: "member",
     });
 
@@ -32,5 +36,22 @@ module.exports = {
       .catch((error) => {
         return res.status(500).json(error);
       });
+  },
+
+  signIn: async (req, res) => {
+    const { username, password } = req.body;
+    const emptyErrors = isEmpty({ username, password });
+
+    if (Object.keys(emptyErrors).length > 0)
+      return res.status(400).json(emptyErrors);
+
+    const user = await User.findOne({ where: { username, password } });
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    const token = jwt.sign({ username }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    return res.status(200).json({ ...user.toJSON(), token });
   },
 };
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const moongose = require("mongoose");
-const { signUp } = require("./controllers/auth");
+const { signUp, signIn } = require("./controllers/auth");
 require("dotenv").config();
 
 app.use(express.urlencoded({ extended: true }));
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.post('/auth/signup', signUp);
+app.get('/auth/signin',signIn);
 
 const mongodbConnection = process.env.MONGODB_CONNECTION_STRING;
 
