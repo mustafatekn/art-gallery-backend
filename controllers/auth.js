cf8583a72abc9ef3006089810bc3ce8d35ee5b28
Mustafa Osman Tekin <mustafa.tekin@demarkelabs.com>
2022-04-10 16:17:03 +0300

    add general validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,19 @@
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+const {isEmpty, isMatched, isEmail} = require('../util/validate');
 
 module.exports = {
   signUp: async (req, res) => {
     const { username, email, password, confirmPassword } = req.body;
+    const emptyErrors = isEmpty({username, email, password});
+    const matchedErrors = isMatched({password, confirmPassword});
+    const emailErrors = isEmail(email);
 
+    if(Object.keys(emptyErrors).length > 0) return res.status(400).json(emptyErrors);
+    if(Object.keys(emailErrors).length > 0) return res.status(400).json(emailErrors);
+    if(Object.keys(matchedErrors).length > 0) return res.status(400).json(matchedErrors);
+    
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = new User({
