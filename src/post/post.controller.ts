21c5b5b7730ed3fa370779f39ae6296ba33af624
Mustafa Osman Tekin <mustafa.tekin@demarkelabs.com>
2022-06-26 21:38:20 +0300

    create post.controller.ts

---
+++ src/post/post.controller.ts
@@ -0,0 +1,63 @@
+import { Request, Response } from 'express';
+const User = require('../models/User')
+const jwt_decode = require('jwt-decode')
+const { isEmpty } = require('../util/validate')
+const Post = require('../models/Post')
+
+module.exports = {
+    createPost: async (req : Request, res : Response) => {
+        const { title, text, url, imageUrl } = req.body
+        const token = req.get('Authorization')
+        const userInfo = jwt_decode(token)
+        const emptyErrors = isEmpty({ title, text, url, imageUrl })
+
+        if (Object.keys(emptyErrors).length > 0)
+            return res.status(400).json(emptyErrors)
+
+        const userFromRequest = await User.findById(userInfo.userId)
+
+        if (!userFromRequest)
+            return res.status(401).json({ error: 'Unauthorized' })
+
+        const post = new Post({
+            title,
+            text,
+            url,
+            imageUrl,
+            user: {
+                id: userInfo.userId,
+                username: userInfo.username,
+                role: userInfo.role,
+            },
+        })
+
+        try {
+            const createdPost = await post.save()
+            return res.status(201).json(createdPost)
+        } catch (error) {
+            return res.status(500).json(error)
+        }
+    },
+
+    deletePost: async (req : Request, res : Response) => {
+        const { id } = req.params
+        const token = req.get('Authorization')
+        if (!token) return res.status(401).json({ error: 'Unauthorized' })
+        const userInfo = jwt_decode(token)
+
+        const userFromRequest = await User.findById(userInfo.userId)
+
+        if (!userFromRequest)
+            return res.status(401).json({ error: 'Unauthorized' })
+
+        const post = await Post.findById(id)
+        if (!post) return res.status(404).json({ error: 'Not Found' })
+
+        try {
+            const deletedPost = await Post.findByIdAndDelete(id)
+            return res.status(200).json(deletedPost)
+        } catch (error) {
+            return res.status(500).json(error)
+        }
+    },
+}
