e44c9b800e429b9dc533ad46ebfdf24902109f52
Mustafa Osman Tekin <mustafa.tekin@demarkelabs.com>
2022-06-27 08:57:15 +0300

    move users to typescript files

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,7 @@ const express = require('express')
 const {
     signUp,
     signIn,
-    createUser,
+    createNewUser,
     deleteUser,
     updateUser,
 } = require('../controllers/auth')
@@ -13,7 +13,7 @@ export const router = express.Router()
 //Auth Routes
 router.post('/auth/signup', signUp)
 router.get('/auth/signin', signIn)
-router.post('/auth/createuser', createUser)
+router.post('/auth/createuser', createNewUser)
 router.delete('/auth/user/:id', deleteUser)
 router.put('/auth/user/:id', updateUser)
 router.post('/post/createPost', createPost)
---
+++ src/user/user.service.ts
@@ -0,0 +1,46 @@
+import User from './user.model'
+
+export const createUser = async ({
+    username,
+    email,
+    password,
+    role,
+}: {
+    username: string
+    email: string
+    password: string
+    role: string
+}) => {
+    const newUser = new User({
+        username,
+        email,
+        password,
+        role,
+    })
+
+    return await newUser.save()
+}
+
+export const getUserById = async (id: string) => {
+    return await User.findById(id)
+}
+
+export const removeUserById = async (id: string) => {
+    return await User.findByIdAndDelete(id)
+}
+
+export const updateUserById = async (
+    id: string,
+    user: {
+        username: string
+        email: string
+        password: string
+        role: string
+    }
+) => {
+    return await User.findByIdAndUpdate(id, user)
+}
+
+export const getUserByUsername = async (username: string) => {
+    return await User.findOne({ username })
+}
