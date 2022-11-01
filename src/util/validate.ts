a8d401ea64d9467a2fff65f6cab8ae7124ad7034
Mustafa Osman Tekin <mustafa.tekin@demarkelabs.com>
2022-11-01 21:35:51 +0300

    fix email format message

--- src/util/validate.ts
+++ src/util/validate.ts
@@ -34,7 +34,7 @@ export const isEmail = (email: string) => {
 
     const emailRegExp =
         /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
-    if (!emailRegExp.test(String(email))) errors.email = 'Email cannot be empty'
+    if (!emailRegExp.test(String(email))) errors.email = 'Email must be in email format.'
 
     return errors
 }
