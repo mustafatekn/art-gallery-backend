80efdc5ed8c6e789e14c2af62076ad5c32d443e2
Mustafa Osman Tekin <mustafa.tekin@demarkelabs.com>
2022-11-13 00:07:08 +0300

    create allow list for cors

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,21 @@ import { Env } from './types'
 
 dotenv.config()
 const app = express()
+
+var allowList = [process.env.FRONTEND_URI, process.env.IYZICO_URI]
+var corsOptions = {
+    origin: function (origin: any, callback: any) {
+        if (allowList.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('This url has not been permitted by CORS.'))
+        }
+    },
+}
+
+app.use(cors(corsOptions))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(cors())
 app.use('/', router)
 
 const mongodbConnection: Env = process.env.MONGODB_CONNECTION_STRING
