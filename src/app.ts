59a1683aabb7092d01c8a24d12ea26890051fb34
Mustafa Osman Tekin <mustafa.tekin@demarkelabs.com>
2022-11-13 08:16:49 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import cors from 'cors'
+// import cors from 'cors'
 import moongose, { ConnectOptions } from 'mongoose'
 import dotenv from 'dotenv'
 import { router } from './routes'
@@ -8,12 +8,23 @@ import { Env } from './types'
 dotenv.config()
 const app = express()
 
-var corsOptions = {
-    origin: [process.env.FRONTEND_LOCAL_URI!, process.env.FRONTEND_URI!],
-    credentials: true,
-};
+// var corsOptions = {
+//     origin: [process.env.FRONTEND_LOCAL_URI!, process.env.FRONTEND_URI!],
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     preflightContinue: false,
+//     optionsSuccessStatus: 204,
+// }
 
-app.use(cors(corsOptions))
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', ["https://art-gallery-frontend.vercel.app", "http://localhost:3000"]) // update to match the domain you will make the request from
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    next()
+})
+
+// app.use(cors(corsOptions))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use('/', router)
