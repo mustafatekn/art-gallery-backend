3ef2e69b553aad6c294eaf22238689bdb9d7683f
Mustafa Osman Tekin <57020656+mustafatekn@users.noreply.github.com>
2024-06-20 01:20:25 +0300

    add new cors config

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const config = {
+    allowedHeaders: ['content-type'],
+    origin: 'http://localhost:3000',
+    credentials: true,
+  };
+
+  app.enableCors(config);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   await app.listen(5000);
 }
