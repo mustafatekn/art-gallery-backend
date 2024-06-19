4adb49fb7df953f291cee65d0e032a9c8ab1daab
Mustafa Osman Tekin <57020656+mustafatekn@users.noreply.github.com>
2024-06-20 01:27:12 +0300

    remove custom cors config

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const config = {
-    allowedHeaders: ['content-type'],
-    origin: 'http://localhost:3000',
-    credentials: true,
-  };
-
-  app.enableCors(config);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   await app.listen(5000);
 }
