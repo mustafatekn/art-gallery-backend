9c3a34777ea42aa2543e125bd0ba34cff49ee093
Mustafa Osman Tekin <57020656+mustafatekn@users.noreply.github.com>
2024-06-20 01:16:07 +0300

    remove cors configuration, use default config

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  const options = {
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true
-  };
-
-  app.enableCors(options);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   await app.listen(5000);
 }
