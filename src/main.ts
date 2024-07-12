a6ca0555d51ed4c04a1eea41167fef598294e540
Mustafa Osman Tekin <57020656+mustafatekn@users.noreply.github.com>
2024-07-12 21:22:35 +0300

    get frontend uri from environment

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://art-gallery-frontend.vercel.app'],
+    origin: process.env.FRONTEND_URI,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     credentials: true,
   });
